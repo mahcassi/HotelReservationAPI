32cc48deba9dc67ba126a7edeaa2079ffc7bb0ff
Maria <mariaed.cassiano@gmail.com>
2023-07-04 21:56:11 -0300

    Add entity ApplicationUser

---
+++ Entity/Entity/Entity/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.Entity
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string Cellphone { get; set; }
+        public int UserType { get; set; }
+    }
+}
