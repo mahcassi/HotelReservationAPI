42dd89366e2dbbd9c4024d36d674ebff34e93e7d
Maria <mariaed.cassiano@gmail.com>
2023-07-05 21:42:33 -0300

    Add validation to AddressHotel

---
+++ Entity/Entity/Validations/AddressHotelValidation.cs
@@ -0,0 +1,36 @@
+﻿using Entity.Entity;
+using FluentValidation;
+
+namespace Entity.Validations
+{
+    public class AddressHotelValidation : AbstractValidator<AddressHotel>
+    {
+        public AddressHotelValidation()
+        {
+            RuleFor(c => c.Complement)
+                .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido")
+                .Length(2, 200).WithMessage("O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caracteres");
+
+            RuleFor(c => c.Neighborhood)
+                .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido")
+                .Length(2, 100).WithMessage("O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caracteres");
+
+            RuleFor(c => c.ZipCode)
+                .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido")
+                .Length(8).WithMessage("O campo {PropertyName} precisa ter {MaxLength} caracteres");
+
+            RuleFor(c => c.City)
+                .NotEmpty().WithMessage("A campo {PropertyName} precisa ser fornecida")
+                .Length(2, 100).WithMessage("O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caracteres");
+
+            RuleFor(c => c.State)
+                .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido")
+                .Length(2, 50).WithMessage("O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caracteres");
+
+            RuleFor(c => c.Number)
+                .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido")
+                .Length(1, 50).WithMessage("O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caracteres");
+        }
+
+    }
+}
