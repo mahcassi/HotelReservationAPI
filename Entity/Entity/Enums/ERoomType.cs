3fe7b0294724aa477df90a6164ecf2be4e04ded3
Maria <mariaed.cassiano@gmail.com>
2023-07-12 21:04:33 -0300

    Created enum RoomType

---
+++ Entity/Entity/Enums/ERoomType.cs
@@ -0,0 +1,28 @@
+﻿using Entity.Entity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.Enums
+{
+    public enum ERoomType
+    {
+        [Description("Quarto Individual")]
+        SingleRoom,
+
+        [Description("Quarto Duplo")]
+        DoubleRoom,
+
+        [Description("Quarto Twin")]
+        TwinRoom,
+
+        [Description("Suite")]
+        Suite,
+
+        [Description("Quarto família")]
+        FamilyRoom,
+    }
+}
