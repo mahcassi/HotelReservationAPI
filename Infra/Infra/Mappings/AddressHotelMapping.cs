4d62c950111b8316a9dd8e71156ef4715fe7f208
Maria <mariaed.cassiano@gmail.com>
2023-07-17 21:54:35 -0300

    Add addressHotelMapping

---
+++ Infra/Infra/Mappings/AddressHotelMapping.cs
@@ -0,0 +1,49 @@
+﻿using Entity.Entity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra.Mappings
+{
+    public class AddressHotelMapping : IEntityTypeConfiguration<AddressHotel>
+    {
+        public void Configure(EntityTypeBuilder<AddressHotel> builder)
+        {
+            builder.HasKey(p => p.Id);
+
+            builder.Property(c => c.Street)
+                .IsRequired()
+                .HasColumnType("varchar(200)");
+
+            builder.Property(c => c.Number)
+                .IsRequired()
+                .HasColumnType("varchar(50)");
+
+            builder.Property(c => c.ZipCode)
+                .IsRequired()
+                .HasColumnType("varchar(8)");
+
+            builder.Property(c => c.Complement)
+                .HasColumnType("varchar(250)");
+
+            builder.Property(c => c.Neighborhood)
+                .IsRequired()
+                .HasColumnType("varchar(100)");
+
+            builder.Property(c => c.City)
+                .IsRequired()
+                .HasColumnType("varchar(100)");
+
+            builder.Property(c => c.State)
+                .IsRequired()
+                .HasColumnType("varchar(50)");
+
+            builder.ToTable("AddressHotel");
+
+        }
+    }
+}
