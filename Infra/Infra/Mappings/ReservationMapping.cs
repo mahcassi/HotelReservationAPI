f0a01fe03a6915c04b6a8ce1302b41b988de196e
Maria <mariaed.cassiano@gmail.com>
2023-07-20 20:53:05 -0300

    Created ReservationMapping

---
+++ Infra/Infra/Mappings/ReservationMapping.cs
@@ -0,0 +1,43 @@
+﻿using Entity.Entity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra.Mappings
+{
+    public class ReservationMapping : IEntityTypeConfiguration<Reservation>
+    {
+        public void Configure(EntityTypeBuilder<Reservation> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(b => b.CheckIn)
+                .IsRequired()
+                .HasColumnType("DateTime");
+
+            builder.Property(b => b.CheckOut)
+                .IsRequired()
+                .HasColumnType("DateTime");
+
+            builder.Property(b => b.NumberPeople)
+                .IsRequired()
+                .HasColumnType("int");
+
+            builder.HasOne(b => b.Guest)
+                .WithMany(h => h.Reservations)
+                .HasForeignKey(b => b.GuestId)
+                .IsRequired();
+
+            builder.HasOne(b => b.Room)
+                .WithOne(h => h.Reservation)
+                .IsRequired();
+
+            builder.ToTable("Reservations");
+        }
+    }
+}
