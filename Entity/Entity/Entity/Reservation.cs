9f9eb50d2eacffb7a65cc15b80e5ee79a4563068
Maria <mariaed.cassiano@gmail.com>
2023-07-20 20:44:54 -0300

    entitity "PaymentReservation" has been deleted because this feature will be implemented in the future

--- Entity/Entity/Entity/Reservation.cs
+++ Entity/Entity/Entity/Reservation.cs
@@ -16,12 +16,9 @@ namespace Entity.Entity
         public DateTime CheckIn { get; set; }
         public DateTime CheckOut { get; set; }
         public int NumberPeople { get; set; }
-        public int PaymentStatus { get; set; }
         public int GuestId { get; set; }
         public Guest Guest { get; set; }
         public int RoomId { get; set; }
         public Room Room { get; set; }
-        public int PaymentReservationId { get; set; }
-        public PaymentReservation PaymentReservation { get; set; }
     }
 }
