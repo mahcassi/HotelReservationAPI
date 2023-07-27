9a7077cb7b84f98180e6381e5832c6d3b7e8fb73
Maria <mariaed.cassiano@gmail.com>
2023-07-27 20:38:26 -0300

    Add interface HotelRepository

---
+++ Domain/Interfaces/Repository/IHotelRepository.cs
@@ -0,0 +1,14 @@
+﻿using Entity.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces.Repository
+{
+    public interface IHotelRepository : IBaseRepository<Hotel>
+    {
+        Task<Hotel> GetHotelAddress(int id);
+    }
+}
