46232a0d1149ffa14aa0cb2d44a2fc4473319907
Maria <mariaed.cassiano@gmail.com>
2023-07-26 22:05:59 -0300

    Created interface BaseRepository

---
+++ Domain/Interfaces/IBaseRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces
+{
+    public interface IBaseRepository<TEntity> : IDisposable where TEntity : class
+    {
+        Task Add(TEntity entity);
+        Task<TEntity> GetById(int id);
+        Task<List<TEntity>> GetAll();
+        Task Update(TEntity entity);
+        Task Remove(int id);
+        Task<IEnumerable<TEntity>> Search(Expression<Func<TEntity, bool>> predicate);
+        Task<int> SaveChanges();
+    }
+}
