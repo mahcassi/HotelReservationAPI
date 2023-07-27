a2bc12365a9f5375b33fb07d3fc3aaa034e83c35
Maria <mariaed.cassiano@gmail.com>
2023-07-27 20:38:05 -0300

    Created interface RoomRepository

---
+++ Domain/Interfaces/Repository/IRoomRepository.cs
@@ -0,0 +1,14 @@
+﻿using Entity.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces.Repository
+{
+    public interface IRoomRepository : IBaseRepository<Room>
+    {
+        Task<Room> GetRoomHotel(int id);
+    }
+}
