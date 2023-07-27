295399f1510e1fc483ad167a05f9f6cbcb4f4b62
Maria <mariaed.cassiano@gmail.com>
2023-07-27 19:48:55 -0300

    Add BaseRepository

--- Infra/Infra/Repository/BaseRepository.cs
+++ Infra/Infra/Repository/BaseRepository.cs
@@ -1,13 +1,70 @@
-﻿using System;
+﻿using Domain.Interfaces;
+using Entity.Entity;
+using Infra.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Infra.Repository
 {
-    public class BaseRepository
+    public abstract class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : BaseEntity, new()
     {
+        protected readonly MyDbContext Db;
+        protected readonly DbSet<TEntity> DbSet;
+
+        public BaseRepository(MyDbContext db)
+        {
+            Db = db;
+            DbSet = db.Set<TEntity>();
+        }
+
+        public async Task Add(TEntity entity)
+        {
+            DbSet.Add(entity);
+            await SaveChanges();
+        }
+
+       
+        public virtual async Task<List<TEntity>> GetAll()
+        {
+            return await DbSet.ToListAsync()
+;        }
+
+        public async Task<TEntity> GetById(int id)
+        {
+            return await DbSet.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<TEntity>> Search(Expression<Func<TEntity, bool>> predicate)
+        {
+            return await DbSet.AsNoTracking().Where(predicate).ToArrayAsync();
+        }
+
+        public virtual async Task Remove(int id)
+        {
+            DbSet.Remove(new TEntity { Id = id });
+            await SaveChanges();
+        }
+
+        public async Task Update(TEntity entity)
+        {
+            DbSet.Update(entity);
+            await SaveChanges();
+        }
+
+        public async Task<int> SaveChanges()
+        {
+            return await Db.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            Db?.Dispose();
+        }
 
     }
 }
