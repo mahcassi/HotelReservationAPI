dea10eb4702e9cf7b08b85d838f62a82a2af2b1f
Maria <mariaed.cassiano@gmail.com>
2023-07-29 21:43:28 -0300

    Update-database

---
+++ Infra/Infra/Migrations/20230815004042_Update-Room-Entity.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infra.Migrations
+{
+    public partial class UpdateRoomEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Number",
+                table: "Rooms",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Number",
+                table: "Rooms");
+        }
+    }
+}
