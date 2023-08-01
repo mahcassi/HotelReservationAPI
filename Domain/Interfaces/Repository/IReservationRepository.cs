618c5058712de10b2218ddbcdbdddde83e395b70
Maria <mariaed.cassiano@gmail.com>
2023-07-31 22:18:57 -0300

    criada IReservationRepository

---
+++ Domain/Interfaces/Repository/IReservationRepository.cs
@@ -0,0 +1,9 @@
+﻿using Entity.Entity;
+
+namespace Domain.Interfaces.Repository
+{
+    public interface IReservationRepository : IBaseRepository<Reservation>
+    {
+        Task<Reservation> GetReservationGuestRoom(int id);
+    }
+}
