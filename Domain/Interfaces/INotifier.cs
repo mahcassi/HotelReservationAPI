8454fe3e9b6fd6bfeaad82172d8a4b0a6473d7e2
Maria <mariaed.cassiano@gmail.com>
2023-08-02 20:48:26 -0300

    Created interface INotifier

---
+++ Domain/Interfaces/INotifier.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Notifications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces
+{
+    public interface INotifier
+    {
+        bool HasNotification();
+        List<Notification> GetNotifications();
+        void Handle(Notification notification);
+    }
+}
