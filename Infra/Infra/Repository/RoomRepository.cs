2212c47ccf381c690c786af4364e264652f24415
Maria <mariaed.cassiano@gmail.com>
2023-08-01 22:24:54 -0300

    Created Room Repository

---
+++ Infra/Infra/Repository/RoomRepository.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Interfaces.Repository;
+using Entity.Entity;
+using Infra.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra.Repository
+{
+    public class RoomRepository : BaseRepository<Room>, IRoomRepository
+    {
+        public RoomRepository(MyDbContext context) : base(context)
+        {
+        }
+
+        public async Task<Room> GetRoomHotel(int id)
+        {
+            return await Db.Rooms.AsNoTracking()
+                .Include(c => c.Hotel)
+                .FirstOrDefaultAsync(c => c.Id == id);
+        }
+    }
+}
