ba42c5116b9ef00d23412b277b11b445d1195495
Maria <mariaed.cassiano@gmail.com>
2023-08-02 20:52:32 -0300

    created class notifier

--- Domain/Notifications/Notifier.cs
+++ Domain/Notifications/Notifier.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Domain.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,27 @@ using System.Threading.Tasks;
 
 namespace Domain.Notifications
 {
-    public class Notifier
+    public class Notifier : INotifier
     {
+        private List<Notification> _notifications;
+
+        public Notifier()
+        {
+            _notifications = new List<Notification>();
+        }
+        public List<Notification> GetNotifications()
+        {
+           return _notifications;
+        }
+
+        public void Handle(Notification notification)
+        {
+            _notifications.Add(notification);
+        }
+
+        public bool HasNotification()
+        {
+            return _notifications.Any();
+        }
     }
 }
