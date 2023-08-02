43c01a4a0c5465ce8d2cfb9a1996bc3b6d3971fd
Maria <mariaed.cassiano@gmail.com>
2023-08-01 22:30:10 -0300

    Created Reservation Repository

---
+++ Infra/Infra/Repository/ReservationRepository.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Interfaces.Repository;
+using Entity.Entity;
+using Infra.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra.Repository
+{
+    public class ReservationRepository : BaseRepository<Reservation>, IReservationRepository
+    {
+        public ReservationRepository(MyDbContext context) : base(context)
+        {
+        }
+
+        public async Task<Reservation> GetReservationGuestRoom(int id)
+        {
+            return await Db.Reservations.AsNoTracking()
+                .Include(c => c.Guest)
+                .Include(c => c.Room)
+                .FirstOrDefaultAsync(c => c.Id == id);
+        }
+    }
+}
