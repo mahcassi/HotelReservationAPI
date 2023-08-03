002b4d55ab543ed797ba063348ce4da165f1a7b7
Maria <mariaed.cassiano@gmail.com>
2023-08-02 21:22:33 -0300

    Created interface HotelService

---
+++ Domain/Interfaces/Services/IHotelService.cs
@@ -0,0 +1,16 @@
+﻿using Entity.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces.Services
+{
+    public interface IHotelService : IDisposable
+    {
+        Task Add(Hotel hotel);
+        Task Update(Hotel hotel);
+        Task Remove(int id);
+    }
+}
