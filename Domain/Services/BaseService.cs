c1aef84bd8b883d4440f0c8f9de9f63c97fd82ca
Maria <mariaed.cassiano@gmail.com>
2023-08-02 21:15:11 -0300

    Created base service to receive notification

---
+++ Domain/Services/BaseService.cs
@@ -0,0 +1,47 @@
+﻿using Domain.Interfaces;
+using Domain.Notifications;
+using Entity.Entity;
+using FluentValidation;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Services
+{
+    public abstract class BaseService
+    {
+        private readonly INotifier _notifier;
+
+        public BaseService(INotifier notifier)
+        {
+            _notifier = notifier;
+        }
+
+        protected void Notify(ValidationResult validationResult)
+        {
+            foreach(var error in validationResult.Errors)
+            {
+                Notify(error.ErrorMessage);
+            }
+        }
+
+        protected void Notify(string errorMessage)
+        {
+            _notifier.Handle(new Notification(errorMessage));
+        }
+
+        protected bool ExecuteValidation<TV, TE>(TV validation, TE entity) where TV : AbstractValidator<TE> where TE : BaseEntity
+        {
+            var validator = validation.Validate(entity);
+
+            if (validator.IsValid) return true;
+
+            Notify(validator);
+
+            return false;
+        }
+    }
+}
