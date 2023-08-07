6cc93d01d043f738670ef6960aa2c15733a6f2ab
Maria <mariaed.cassiano@gmail.com>
2023-08-06 22:00:11 -0300

    Added HotelAmenitiesDTO

---
+++ API/DTO/HotelAmenitiesDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.DTO
+{
+    public class HotelAmenitiesDTO
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public int HotelId { get; set; }
+    }
+}
