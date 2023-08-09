775af1f00fba7055be6adb36919ec5a6f0a99263
Maria <mariaed.cassiano@gmail.com>
2023-08-08 21:33:59 -0300

    Created HotelController

---
+++ API/Controllers/HotelsController.cs
@@ -0,0 +1,29 @@
+﻿using API.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using AutoMapper;
+using Domain.Interfaces.Repository;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HotelsController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly IHotelRepository _hotelRepository;
+
+        public HotelsController(IHotelRepository hotelRepository, IMapper mapper)
+        {
+            _hotelRepository = hotelRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<HotelDTO>> ObterTodos()
+        {
+            var hotel = _mapper.Map<IEnumerable<HotelDTO>>(await _hotelRepository.GetAll());
+            return hotel;
+        }
+    }
+}
