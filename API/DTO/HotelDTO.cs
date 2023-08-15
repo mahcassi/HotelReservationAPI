ed7894b8fe7cc9ae8ace585182600d0d4fd9351b
Maria <mariaed.cassiano@gmail.com>
2023-08-14 22:01:42 -0300

    Added Address and Room in HotelDTO and DataAnnotations in RoomDTO

--- API/DTO/HotelDTO.cs
+++ API/DTO/HotelDTO.cs
@@ -14,5 +14,8 @@ namespace API.DTO
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         [StringLength(14, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 11)]
         public string CNPJ { get; set; }
+
+        public AddressHotelDTO AddressHotel { get; set; }
+        public IEnumerable<RoomDTO> Rooms { get; set; }
     }
 }
--- API/DTO/RoomDTO.cs
+++ API/DTO/RoomDTO.cs
@@ -6,12 +6,27 @@ namespace API.DTO
 {
     public class RoomDTO
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
         public ERoomType RoomType { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
         public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
         public string Number { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
         public bool Availability { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
         public string Size { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
         public IEnumerable<RoomAmenities> Amenities { get; set; }
+
         public Reservation Reservation { get; set; }
 
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
