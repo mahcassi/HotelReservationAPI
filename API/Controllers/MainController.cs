8d25cbf2d8d0137e06adbb61a39126d67c991409
Maria <mariaed.cassiano@gmail.com>
2023-08-15 21:18:34 -0300

    Created main controller to display response

---
+++ API/Controllers/MainController.cs
@@ -0,0 +1,60 @@
+﻿using Domain.Interfaces;
+using Domain.Notifications;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace API.Controllers
+{
+    public abstract class MainController : ControllerBase
+    {
+        private readonly INotifier _notifier;
+
+        public MainController(INotifier notifier)
+        {
+            _notifier = notifier;
+        }
+
+        protected bool OperationIsValid()
+        {
+            return !_notifier.HasNotification();
+        }
+
+        protected ActionResult CustomResponse(object result = null)
+        {
+            if (OperationIsValid())
+            {
+                return Ok(new { success = true, data = result });
+            }
+
+            return BadRequest(new
+            {
+                success = false,
+                errors = _notifier.GetNotifications().Select(m => m.Message)
+            });
+        }
+
+        protected ActionResult CustomResponse(ModelStateDictionary modelState)
+        {
+            if (!modelState.IsValid) NotifierErrorModel(modelState);
+
+            return CustomResponse();
+        }
+
+        protected void NotifierErrorModel(ModelStateDictionary modelState)
+        {
+            var erros = modelState.Values.SelectMany(e => e.Errors);
+
+            foreach (var erro in erros)
+            {
+                var errorMsg = erro.Exception == null ? erro.ErrorMessage : erro.Exception.Message;
+                NotifierError(errorMsg);
+            }
+        }
+
+        protected void NotifierError(string mensagem)
+        {
+            _notifier.Handle(new Notification(mensagem));
+        }
+
+    }
+}
