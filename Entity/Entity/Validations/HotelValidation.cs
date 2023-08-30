208b4674f6983b6e90ad02b0086cfe6f5d8236a6
Maria <mariaed.cassiano@gmail.com>
2023-08-29 21:03:43 -0300

    Remove CNPJ validation

--- Entity/Entity/Validations/HotelValidation.cs
+++ Entity/Entity/Validations/HotelValidation.cs
@@ -17,10 +17,10 @@ namespace Entity.Validations
                 .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido")
                 .Length(2, 200).WithMessage("O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caracteres");
 
-            RuleFor(f => f.CNPJ.Length).Equal(CnpjValidacao.TamanhoCnpj)
-                    .WithMessage("O campo Documento precisa ter {ComparisonValue} caracteres e foi fornecido {PropertyValue}.");
-            RuleFor(f => CnpjValidacao.Validar(f.CNPJ)).Equal(true)
-                .WithMessage("O documento fornecido é inválido.");
+            //RuleFor(f => f.CNPJ.Length).Equal(CnpjValidacao.TamanhoCnpj)
+            //        .WithMessage("O campo Documento precisa ter {ComparisonValue} caracteres e foi fornecido {PropertyValue}.");
+            //RuleFor(f => CnpjValidacao.Validar(f.CNPJ)).Equal(true)
+            //    .WithMessage("O documento fornecido é inválido.");
         }
     }
 }
