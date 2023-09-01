4391767da0fb1376a4e2fede0b5194a45355210a
Maria <mariaed.cassiano@gmail.com>
2023-09-01 08:18:56 -0300

    adjust DTO's

--- API/DTO/HotelResponseDTO.cs
+++ API/DTO/HotelResponseDTO.cs
@@ -7,12 +7,8 @@ namespace API.DTO
         [Key]
         public int Id { get; set; }
 
-        //[Required(ErrorMessage = "O campo {0} é obrigatório")]
-        //[StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
         public string Name { get; set; }
 
-        //[Required(ErrorMessage = "O campo {0} é obrigatório")]
-        //[StringLength(14, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 11)]
         public string CNPJ { get; set; }
 
         public AddressHotelDTO AddressHotel { get; set; }
