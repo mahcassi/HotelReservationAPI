35b69048b8af2293f4583ec630e228b0165aeb49
Maria Eduarda <mariaed.cassiano@gmail.com>
2023-09-20 21:38:40 -0300

    Created RoomAmentiesMapping

---
+++ Infra/Infra/Mappings/AmenityRoomMapping.cs
@@ -0,0 +1,22 @@
+﻿using Entity.Entity;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infra.Mappings
+{
+    public class AmenityRoomMapping : IEntityTypeConfiguration<AmenityRoom>
+    {
+        public void Configure(EntityTypeBuilder<AmenityRoom> builder)
+        {
+            builder.HasKey(b => b.Id);
+
+            builder.Property(b => b.Name)
+                .IsRequired()
+                .HasColumnType("varchar(100)");
+
+            builder.HasMany(h => h.RoomAmenities)
+              .WithOne(ha => ha.AmenityRoom)
+              .HasForeignKey(ha => ha.AmenityId);
+        }
+    }
+}
---
+++ Infra/Infra/Mappings/RoomAmenitiesMapping.cs
@@ -0,0 +1,22 @@
+﻿using Entity.Entity;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infra.Mappings
+{
+    public class RoomAmenitiesMapping : IEntityTypeConfiguration<RoomAmenity>
+    {
+        public void Configure(EntityTypeBuilder<RoomAmenity> builder)
+        {
+            builder.HasKey(ha => new { ha.RoomId, ha.AmenityId });
+
+            builder.HasOne(ha => ha.Room)
+                .WithMany(h => h.RoomAmenities)
+                .HasForeignKey(ha => ha.RoomId);
+
+            builder.HasOne(ha => ha.AmenityRoom)
+                .WithMany(a => a.RoomAmenities)
+                .HasForeignKey(ha => ha.AmenityId);
+        }
+    }
+}
