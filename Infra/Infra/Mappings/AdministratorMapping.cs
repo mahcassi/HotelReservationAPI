19d0b62c51d00864deb0620c5cb73e9b3ad34ef2
Maria Eduarda <mariaed.cassiano@gmail.com>
2023-10-03 22:02:08 -0300

    Added AdministratorMapping

---
+++ Infra/Infra/Mappings/AdministratorMapping.cs
@@ -0,0 +1,21 @@
+﻿using Entity.Entity;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra.Mappings
+{
+    public class AdministratorMapping : IEntityTypeConfiguration<Administrator>
+    {
+        public void Configure(EntityTypeBuilder<Administrator> builder)
+        {
+            builder.HasKey(b => b.Id);
+
+            builder.ToTable("AdministratorUser");
+        }
+    }
+}
