4e38a0f1b6023c3c666f0abc02b174e1ca16019f
Maria Eduarda <mariaed.cassiano@gmail.com>
2023-10-03 22:01:40 -0300

    Created ApplicationDbContext

---
+++ Infra/Infra/Context/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Entity.Entity;
+using Infra.Context;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<Administrator>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
